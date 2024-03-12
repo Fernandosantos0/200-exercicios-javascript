cfcdf7ffed9e23de3f41bd4d786c1a3d4b456877
Fernando Santos <fernandomeca7@gmail.com>
2024-03-12 16:08:21 -0300

    Merge até o exercício 125

--- exercicio94 - array/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function findByStringLength(arrays, n) {
-    return arrays.filter(string => string.length > 4);
-}
-
-const strings = ['Canadá', 'Espanha', 'Chile', 'Equador', 'Uruguai', 'Peru', 'Cuba', 'Guine', 'França', 'Egito'];
-
-console.log(findByStringLength(strings, 6));
